e1a3f06a4ab00d29ea337f2956c101e218613eeb
Lili Bourgeois <lbourgeois90@gmail.com>
2019-05-20 11:07:45 -0500

    revised registration and login- multitransaction for post to user, farm_registry, and people

--- server/routes/setupFarm.router.js
+++ server/routes/setupFarm.router.js
@@ -11,15 +11,22 @@ router.post('/', rejectUnauthenticated, (req,res) => {
     let user_id = req.user.user_id;
     console.log('New Farm is:', newFarm);
     console.log('User is:', user_id);
-    const sqlText= `INSERT INTO "farm_registry" ("farm_name", "address", "city", "state", "zip_code", "user_id") VALUES ($1, $2, $3, $4, $5, $6)`
-    pool.query(sqlText, [newFarm.farm_name, newFarm.address, newFarm.city, newFarm.state, newFarm.zip_code, user_id])
-    .then( (result) => {
-        res.sendStatus(201);
-    })
-    .catch( (error) => {
-        console.log('ERROR in SETUPFARM POST', error);
-        res.sendStatus(500);
-    })
+    let registrationCode = req.body.registrationCode;
+    console.log(registrationCode);
+    // if ( registrationCode == 122090 )
+    //     {const sqlText= `INSERT INTO "farm_registry" ("farm_name", "address", "city", "state", "zip_code", "user_id") VALUES ($1, $2, $3, $4, $5, $6)`
+    //     pool.query(sqlText, [newFarm.farm_name, newFarm.address, newFarm.city, newFarm.state, newFarm.zip_code, user_id])
+    //     .then( (result) => {
+    //         res.sendStatus(201);
+    //     })
+    //     .catch( (error) => {
+    //         console.log('ERROR in SETUPFARM POST', error);
+    //         res.sendStatus(500);
+    //     })
+    // }
+    // else {
+    //     res.sendStatus(500);
+    // }
 })
 
 
