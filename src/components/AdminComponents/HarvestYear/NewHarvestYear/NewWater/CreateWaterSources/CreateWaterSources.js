04aa9db59e2592995122f06b1bb5b4a6a32553aa
Chris Borgen <borgen_12@hotmail.com>
2019-05-20 10:27:36 -0500

    creating inputs and buttons

--- src/components/AdminComponents/HarvestYear/NewHarvestYear/NewWater/CreateWaterSources/CreateWaterSources.js
+++ src/components/AdminComponents/HarvestYear/NewHarvestYear/NewWater/CreateWaterSources/CreateWaterSources.js
@@ -4,16 +4,27 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import './CreateWaterSources.css'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button'
+import FormControl from '@material-ui/core/FormControl'
 
 
 class CreateWaterSources extends Component {
 
 
   state= {
-    
+    waterSource:"",
   }
 
-
+  handleInputChangeFor = propertyName => (event) => {
+    this.setState({
+      newUser: {
+        ...this.state.newUser,
+        [propertyName]: event.target.value,
+        
+      }
+    });
+  }
 
 
   render() {
@@ -23,10 +34,24 @@ class CreateWaterSources extends Component {
             <Typography variant="h6" gutterBottom>
                 Create Water Sources
             </Typography>
-            <Grid container spacing={24}>
+            <Grid container spacing={24}
+            container
+            direction="column"
+            justify="center"
+            alignItems="center">
                
                 <Grid item xs={12} sm={6}>
-                   
+                  <FormControl>
+                    <TextField label="Water Sources" variant="outlined" color="primary"
+                      onChange={this.handleInputChangeFor('registrationCode')}
+                      type="password"
+                      value={this.state.newUser.registrationCode}
+                    >
+                    </TextField>
+                    <Button size="large" color="primary" onClick={this.registerUser} >Register</Button>
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12} sm={6}>
                 </Grid>
 
             </Grid>
