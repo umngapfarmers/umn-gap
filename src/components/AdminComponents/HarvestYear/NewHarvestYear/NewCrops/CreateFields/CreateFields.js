f309276911717747b006e104fce234e87adc4a56
Chris Borgen <borgen_12@hotmail.com>
2019-05-20 16:14:12 -0500

    filled createfields, moving on to sagas

--- src/components/AdminComponents/HarvestYear/NewHarvestYear/NewCrops/CreateFields/CreateFields.js
+++ src/components/AdminComponents/HarvestYear/NewHarvestYear/NewCrops/CreateFields/CreateFields.js
@@ -10,24 +10,74 @@ class CreateFields extends Component {
 
 
   state= {
-    
+    newField: {
+      name: '',
+    }
   }
 
+  handleInputChangeFor = propertyName => (event) => {
+    this.setState({
+      newField: {
+        ...this.setState,
+        [propertyName]: event.target.value,
 
+      }
+    });
+  }
 
+  addFieldSource = (event) => {
+    event.preventDefault();
+    this.props.dispatch({ type: 'ADD_FIELD_SOURCE', payload: this.state.newField })
+    this.setState({
+      newField: {
+        name: '',
+      }
+    })
+  }
+
+  removeCropSource = () => {
+    this.props.dispatch({ type: 'REMOVE_FIELD_SOURCE', payload: this.state.farm_field_id })
+
+  }
 
   render() {
     const {classes} = this.props;
     return (
       <React.Fragment>
-            <Typography variant="h6" gutterBottom>
-                Create Fields
-            </Typography>
-            <Grid container spacing={24}>
+            
+            <Grid container spacing={24}
+          container
+          direction="column"
+          justify="center"
+          alignItems="center">
                
                 <Grid item xs={12} sm={6}>
-                   
+            <Typography variant="h6" gutterBottom>
+              Create Fields
+            </Typography>
                 </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField label="Crops to track" variant="outlined" color="primary"
+              onChange={this.handleInputChangeFor('name')}
+              value={this.state.newField.type}
+            >
+            </TextField>
+            <Button size="large" color="primary" onClick={this.addFieldSource} >Add</Button>
+
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <ul> My Fields:</ul>
+            {
+              this.props.reduxState.cropSetup.fieldSetup.map(crop =>
+                <li key={crop.farm_field_id}>{crop.farm_field_name}
+                  <Button size="large" color="primary" onClick={this.removeFieldSource} >
+                    Remove
+                </Button>
+                </li>
+              )
+            }
+
+          </Grid>
 
             </Grid>
            
