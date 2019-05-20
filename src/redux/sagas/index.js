43c51d79b773d6c2e32df4cbd1318839204b70f6
wabens <waltkbenson@gmail.com>
2019-05-20 16:16:41 -0500

    label code in reducer

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -2,7 +2,8 @@ import { all } from 'redux-saga/effects';
 import loginSaga from './loginSaga';
 import registrationSaga from './registrationSaga';
 import userSaga from './userSaga';
-import setupSaga from './setupSaga'
+import setupSaga from './setupSaga';
+import getLabelCodeSaga from './getLabelCodeSaga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -17,5 +18,6 @@ export default function* rootSaga() {
     registrationSaga(),
     userSaga(),
     setupSaga(),
+    getLabelCodeSaga()
   ]);
 }
