6e4a69f915bd8d4322c879b7643c433da557cd9d
Lili Bourgeois <lbourgeois90@gmail.com>
2019-05-20 10:00:52 -0500

    removed auth

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -29,9 +29,9 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <Switch>
+          {/* <Switch> */}
             {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-            <Redirect exact from="/" to="/home" />
+            {/* <Redirect exact from="/" to="/home" /> */}
             {/* Visiting localhost:3000/about will show the about page.
             This is a route anyone can see, no login necessary */}
             <Route
@@ -43,14 +43,14 @@ class App extends Component {
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
             Even though it seems like they are different pages, the user is always on localhost:3000/home */}
-            <ProtectedRoute
+            <Route
               exact
               path="/home"
               component={UserPage}
             />
             {/* This works the same as the other protected route, except that if the user is logged in,
             they will see the info page instead. */}
-            <ProtectedRoute
+            <Route
               exact
               path="/info"
               component={InfoPage}
@@ -58,8 +58,8 @@ class App extends Component {
             
 
             {/* If none of the other routes matched, we will show a 404. */}
-            <Route render={() => <h1>404</h1>} />
-          </Switch>
+            {/* <Route render={() => <h1>404</h1>} /> */}
+          {/* </Switch> */}
         </div>
       </Router>
   )}
