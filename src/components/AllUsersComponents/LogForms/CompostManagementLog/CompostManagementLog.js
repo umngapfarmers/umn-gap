0a546db398fbf28046851f529b201046ea3a9b73
Lili Bourgeois <lbourgeois90@gmail.com>
2019-05-21 16:15:46 -0500

    merge this we almost lost it

---
+++ src/components/AllUsersComponents/LogForms/CompostManagementLog/CompostManagementLog.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import './CompostManagementLog.css'
+
+
+class CompostManagementLog extends Component {
+
+
+  state= {
+    
+  }
+
+
+
+
+  render() {
+    const {classes} = this.props;
+    return (
+      <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+         Compost Management Log
+      </Typography>
+      <Grid container spacing={24}>
+         
+          <Grid item xs={12} sm={6}>
+             
+          </Grid>
+
+      </Grid>
+     
+  </React.Fragment>
+    );
+  }
+}
+
+const styles = theme => ({
+  container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+  },
+});
+
+
+
+const mapReduxStateToProps = (reduxState) => ({
+  reduxState,
+});
+
+export default connect( mapReduxStateToProps )(withStyles(styles)(CompostManagementLog));
