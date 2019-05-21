0c148dbb58ca34efb0c311e6a01b0a52746f08df
Lili Bourgeois <lbourgeois90@gmail.com>
2019-05-21 11:18:08 -0500

    revision of database

--- GAP.sql
+++ GAP.sql
@@ -20,9 +20,10 @@ CREATE TABLE "user"
 (
   "user_id" SERIAL PRIMARY KEY,
   "username" varchar(50) NOT NULL,
-  "password" VARCHAR(50) NOT NULL,
+  "password" VARCHAR(255) NOT NULL,
   "user_role" VARCHAR (255) NOT NULL,
-  "farm_registry_id" INT REFERENCES "farm_registry"
+  "farm_registry_id" INT REFERENCES "farm_registry",
+  "current_harvest_year" INT REFERENCES "harvest_year"
 );
 
 CREATE TABLE "harvest_year"
