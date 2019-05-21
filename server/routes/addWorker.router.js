225863b3cdfdbcf3548cebe8a5a034449b4ee88c
kashsid <kashifsiddique@gmail.com>
2019-05-21 11:55:07 -0500

    implements conditional rendering on worker form

--- server/routes/addWorker.router.js
+++ server/routes/addWorker.router.js
@@ -5,22 +5,56 @@ const router = express.Router();
 
 
 
-router.post('/', rejectUnauthenticated, (req, res) => {
-    console.log('in Add Worker Router');
-    let newWorker = req.body;
-    //let user_id = req.user.user_id;
-    console.log('New Worker is:', newWorker);
-    //console.log('User is:', user_id);
-    const sqlText = `INSERT INTO "farm_registry" ("farm_name", "address", "city", "state", "zip_code", "user_id") VALUES ($1, $2, $3, $4, $5, $6)`
-    pool.query(sqlText, [newFarm.farm_name, newFarm.address, newFarm.city, newFarm.state, newFarm.zip_code, user_id])
-        .then((result) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
+    // Send back user object from the session (previously queried from the database)
+    res.send(req.user);
+});
+
+// Handles POST request with new user data
+// The only thing different from this and every other post we've seen
+// is that the password gets encrypted before being inserted
+router.post('/addworker', async (req, res) => {
+    const client = await pool.connect();
+    console.log('in add worker');
+    const username = req.body.username;
+    const password = encryptLib.encryptPassword(req.body.password);
+    const user_role = req.body.user_role;
+    let registrationCode = req.body.registrationCode;
+    console.log(registrationCode);
+    if (registrationCode == 122090) {
+        try {
+            //    console.log(req.body);
+            const username = req.body.username;
+            const password = encryptLib.encryptPassword(req.body.password);
+            const user_role = req.body.user_role;
+            const person_first = req.body.person_first;
+            const person_last = req.body.person_last;
+            const person_status = req.body.person_status;
+            let registrationCode = req.body.registrationCode;
+            console.log(registrationCode);
+
+            const usernameQuery = `INSERT INTO "user" (username, password, user_role) VALUES ($1, $2, $3) RETURNING user_id`;
+            const personQuery = `INSERT INTO "person" ("person_first", "person_last", "person_status", "user_id") VALUES ($1, $2, $3, $4)`;
+            await client.query('BEGIN')
+            const userInsertResults = await client.query(usernameQuery, [username, password, user_role]);
+            const user_id = userInsertResults.rows[0].user_id;
+            console.log(user_id);
+
+            const personInsertResults = await client.query(personQuery, [person_first, person_last, person_status, user_id]);
+            await client.query('COMMIT')
             res.sendStatus(201);
-        })
-        .catch((error) => {
-            console.log('ERROR in SETUPFARM POST', error);
+        } catch (error) {
+            await client.query('ROLLBACK')
+            console.log('Error Registering', error);
             res.sendStatus(500);
-        })
-})
+        } finally {
+            client.release()
+        }
+    }
+    else {
+        res.sendStatus(500);
+    }
+});
 
 
 
--- src/components/WorkerDashboard/AddWorker.js
+++ src/components/WorkerDashboard/AddWorker.js
@@ -1,149 +1,225 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import Grid from '@material-ui/core/Grid';
-import FormControl from '@material-ui/core/FormControl'
-import Button from '@material-ui/core/Button'
-
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import { withStyles } from '@material-ui/core/styles';
-import NavBar from '../NavBar/NavBar';
-
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Grid from "@material-ui/core/Grid";
+import FormControl from "@material-ui/core/FormControl";
+import Button from "@material-ui/core/Button";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import { withStyles } from "@material-ui/core/styles";
+import NavBar from "../NavBar/NavBar";
 
 const styles = theme => ({
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    textField: {
-        marginLeft: theme.spacing.unit,
-        // marginRight: theme.spacing.unit,
-        //width: 200,
-    },
-    dense: {
-        marginTop: 19,
-    },
-    menu: {
-        width: 200,
-    },
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  textField: {
+    marginLeft: theme.spacing.unit
+    // marginRight: theme.spacing.unit,
+    //width: 200,
+  },
+  dense: {
+    marginTop: 19
+  },
+  menu: {
+    width: 200
+  }
 });
 class AddWorker extends Component {
+  state = {
+    UserName: "",
+    Password: "",
+    FirstName: "",
+    LastName: "",
+    roleSelect: ""
+  };
+  handleSelect = event => {
+    this.setState({ [event.target.name]: event.target.value });
+    //let roleSelect = event.target.value;
+    //console.log('selected role',this.state.roleSelect)
+  };
+  // handles on inputs on form and sets state
+  handleChange = property => event => {
+    this.setState({
+      ...this.state,
+      [property]: event.target.value
+    });
+    console.log("in handle change", event.target.value);
+  };
 
-    state = {
-        UserName: "",
-        Password: "",
-        FirstName: "",
-        LastName: ""
-};
+  // handles form submit button, sends post dispatch to redux with payload of all selected form inputs + clears form
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log("in handle submit", this.state);
 
-    // handles on inputs on form and sets state
-    handleChange = property => event => {
-        this.setState({
-            ...this.state,
-            [property]: event.target.value
-        });
-        console.log("in handle change", event.target.value);
-    };
+    this.props.dispatch({ type: "POST_WORKER", payload: this.state });
+    this.setState({
+      UserName: "",
+      Password: "",
+      FirstName: "",
+      LastName: "",
+      roleSelect: ""
+    });
+    this.props.history.push("/");
+  };
 
-    // handles form submit button, sends post dispatch to redux with payload of all selected form inputs + clears form
-    handleSubmit = (event) => {
-        event.preventDefault();
-        console.log("in handle submit", this.state);
-
-        this.props.dispatch({ type: "POST_WORKER", payload: this.state });
-        this.setState({
-            UserName: "",
-            Password: "",
-            FirstName: "",
-            LastName: ""
-
-        });
-        this.props.history.push('/');
-    };
-
-    render() {
-        const { classes } = this.props;
+  render() {
+    const { classes } = this.props;
+    console.log("selected role", this.state.roleSelect);
 
     return (
-
-        <React.Fragment>
-            <NavBar/>
-            <Typography variant="h6" gutterBottom>
-                Registration
-      </Typography>
-            <FormControl
-                ref="form"
-                //justify="center"
-                //alignItems="center"
-                fullWidth
-                onSubmit={this.handleSubmit}
-                onError={errors => console.log(errors)}
-           >
-            <Grid container spacing={24}
-                container
-                direction="column"
-                justify="center"
-                alignItems="center"
-            >
-               
-                
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="uName"
-                        name="uName"
-                        label="User Name"
-                        fullWidth
-                        autoComplete="User Name"
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="password"
-                        name="password"
-                        label="password"
-                        fullWidth
-                        autoComplete="password"
-                    />
-                </Grid>
-                <Grid item xs={8} sm={6}>
-                    <TextField
-                        required
-                        id="firstName"
-                        name="firstName"
-                        label="First name"
-                        //fullWidth
-                        autoComplete="fname"
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="lastName"
-                        name="lastName"
-                        label="Last name"
-                        fullWidth
-                        autoComplete="lname"
-                    />
-                </Grid>
-                    <Grid item xs={8} sm={6} >
-                        <FormControl>
-                            <Button onClick={this.handleSubmit}>Submit</Button>
-                        </FormControl>
-                    </Grid>
+      <React.Fragment>
+        <NavBar />
+        <Typography variant="h6" gutterBottom>
+          Registration
+        </Typography>
+        <FormControl
+          ref="form"
+          //justify="center"
+          //alignItems="center"
+          fullWidth
+          className={classes.selectFormControl}
+          onSubmit={this.handleSubmit}
+          onError={errors => console.log(errors)}
+        >
+          <Grid
+            container
+            spacing={24}
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+          >
+            <Grid item xs={12} sm={6}>
+              <label>Select Role</label>
+              <FormControl fullWidth className={classes.selectFormControl}>
+                <Select
+                  MenuProps={{
+                    className: classes.selectMenu
+                  }}
+                  classes={{
+                    select: classes.select
+                  }}
+                  value={this.state.roleSelect}
+                  onChange={this.handleSelect}
+                  inputProps={{
+                    name: "roleSelect",
+                    id: "role-select"
+                  }}
+                >
+                  <MenuItem
+                    classes={{
+                      root: classes.selectMenuItem,
+                      selected: classes.selectMenuItemSelected
+                    }}
+                    value="Admin"
+                  >
+                    Admin
+                  </MenuItem>
+                  <MenuItem
+                    classes={{
+                      root: classes.selectMenuItem,
+                      selected: classes.selectMenuItemSelected
+                    }}
+                    value="Worker"
+                  >
+                    Worker
+                  </MenuItem>
+                  <MenuItem
+                    classes={{
+                      root: classes.selectMenuItem,
+                      selected: classes.selectMenuItemSelected
+                    }}
+                    value="Employee"
+                  >
+                    Employee
+                  </MenuItem>
+                </Select>
+              </FormControl>
             </Grid>
-            </FormControl>
-        </React.Fragment>
 
+            <Grid item xs={12} sm={6}>
+              {this.state.roleSelect !== "Employee" ? (
+                <TextField
+                  required
+                  id="uName"
+                  name="uName"
+                  label="User Name"
+                  disabled={false}
+                  fullWidth
+                  autoComplete="User Name"
+                />
+              ) : (
+                <TextField
+                  required
+                  id="uName"
+                  name="uName"
+                  label="User Name"
+                  disabled
+                  fullWidth
+                  autoComplete="User Name"
+                />
+              )}
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              {this.state.roleSelect !== "Employee" ? (
+                <TextField
+                  required
+                  id="password"
+                  name="password"
+                  label="Password"
+                  disabled={false}
+                  fullWidth
+                  autoComplete="Password"
+                />
+              ) : (
+                <TextField
+                  required
+                  id="password"
+                  name="password"
+                  label="Password"
+                  disabled
+                  fullWidth
+                  autoComplete="Password"
+                />
+              )}
+            </Grid>
+            <Grid item xs={8} sm={6}>
+              <TextField
+                required
+                id="firstName"
+                name="firstName"
+                label="First name"
+                //fullWidth
+                autoComplete="fname"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="lastName"
+                name="lastName"
+                label="Last name"
+                fullWidth
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={8} sm={6}>
+              <FormControl>
+                <Button onClick={this.handleSubmit}>Submit</Button>
+              </FormControl>
+            </Grid>
+          </Grid>
+        </FormControl>
+      </React.Fragment>
     );
-}
+  }
 }
 
-const mapReduxStateToProps = (reduxState) => ({
-    reduxState,
+const mapReduxStateToProps = reduxState => ({
+  reduxState
 });
 
 export default connect(mapReduxStateToProps)(withStyles(styles)(AddWorker));
-
