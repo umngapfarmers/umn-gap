c10666b5507b97d7cdf60edaf856c8c746f9892a
Chris Borgen <borgen_12@hotmail.com>
2019-05-21 00:42:48 -0500

    changing index files

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,6 +4,7 @@ import loginMode from './loginModeReducer';
 import user from './userReducer';
 import waterSetup from './waterSetup';
 import cropSetup from './cropSetup';
+import labelCode from './labelCodesReducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -17,6 +18,7 @@ const rootReducer = combineReducers({
   user, // will have an id and username if someone is logged in
   waterSetup,
   cropSetup,
+  labelCode
 });
 
 export default rootReducer;
