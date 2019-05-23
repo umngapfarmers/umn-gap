f7c2b1991349b44511dbf6c494f662d5da3fec20
wabens <waltkbenson@gmail.com>
2019-05-23 13:24:09 -0500

    manure and compost is importable

--- server/routes/import.harvest.router.js
+++ server/routes/import.harvest.router.js
@@ -11,6 +11,7 @@ router.post('/', async (req, res) => {
     const harvest_year_id = 1;   // CHANGE ME -----------
     const newHarvestId = req.body.new_harvest_id;
 
+    // select and insert queries, as well as property names for each table to be imported
     const getCropQuery = `SELECT * FROM "farm_crop" WHERE "harvest_year_id" = $1;`;
     const insertCropQuery = ` INSERT INTO "farm_crop"("farm_crop_type", "harvest_year_id", "farm_crop_status") VALUES($1, $2, $3);`;
     const cropKeys = ["farm_crop_type", "harvest_year_id", "farm_crop_status"];
@@ -23,6 +24,19 @@ router.post('/', async (req, res) => {
     const insertLabelCodeQuery = `INSERT INTO "label_code" ("farm_crop_id", "farm_field_id", "label_code_text", "harvest_year_id") VALUES ($1, $2, $3, $4);`;
     const labelCodeKeys = ["farm_crop_id", "farm_field_id", "label_code_text", "harvest_year_id"];
 
+    const getFarmManureQuery = `SELECT * FROM "farm_manure" WHERE "harvest_year_id" = $1;`;
+    const insertFarmManureQuery = ` INSERT INTO "farm_manure" ("farm_manure_date", "farm_manure_description", "farm_manure_rate", "label_code_id", "harvest_year_id","farm_manure_status") 
+                        VALUES($1, $2, $3, $4, $5, $6);`;
+    const farmManureKeys = ["farm_manure_date", "farm_manure_description", "farm_manure_rate", "label_code_id", "harvest_year_id", "farm_manure_status"];
+
+    const getFarmCompost = `SELECT * FROM "farm_compost" WHERE "harvest_year_id" = $1;`;
+    const insertFarmCompostQuery = ` INSERT INTO "farm_compost"("farm_compost_name", "farm_compost_date", "farm_compost_description", "user_id", "harvest_year_id","farm_compost_status")
+                                    VALUES($1, $2, $3, $4, $5, $6);`;
+    const farmCompostKeys = ["farm_compost_name", "farm_compost_date", "farm_compost_description", "user_id", "harvest_year_id", "farm_compost_status"];
+    
+    const getFarmWaterSource = `SELECT * FROM "farm_water_source" WHERE "harvest_year_id" = $1;`;
+
+    const getFarmWater = `SELECT * FROM "farm_water" WHERE "harvest_year_id" = $1;`;
 
     // mutates previous year info by removing ids and updating to new harvest year
     // after item is mutated it is posted to the db
@@ -52,7 +66,14 @@ router.post('/', async (req, res) => {
             await changeAndPost(fieldResult.rows, 'farm_field_id', insertFieldQuery, fieldKeys);
             
             const labelCodeResult = await client.query(getLabelCodeQuery, [harvest_year_id]);
-            await changeAndPost(labelCodeResult.rows, 'farm_field_id', insertLabelCodeQuery, labelCodeKeys);
+            await changeAndPost(labelCodeResult.rows, 'label_code_id', insertLabelCodeQuery, labelCodeKeys);
+
+            const farmManureResult = await client.query(getFarmManureQuery, [harvest_year_id]);
+            await changeAndPost(farmManureResult.rows, 'farm_manure_id', insertFarmManureQuery, farmManureKeys);
+
+            const farmCompostResult = await client.query(getFarmCompost, [harvest_year_id]);
+            await changeAndPost(farmCompostResult.rows, 'farm_manure_id', insertFarmCompostQuery, farmCompostKeys);
+
 
             await client.query('COMMIT')
             res.sendStatus(200);
