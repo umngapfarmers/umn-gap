5b637977b63587d5f6241b52c417cf2a33677b01
wabens <waltkbenson@gmail.com>
2019-05-23 19:47:47 -0500

    fixed manure and compost current harvest year

--- GAP.sql
+++ GAP.sql
@@ -114,7 +114,6 @@ CREATE TABLE "farm_compost"
   "farm_compost_name" varchar (200) NOT NULL,
   "farm_compost_date" timestamp,
   "farm_compost_description" varchar (250),
-  "user_id" INT REFERENCES "user",
   "harvest_year_id" INT REFERENCES "harvest_year",
   "farm_compost_status" boolean DEFAULT true
 );
