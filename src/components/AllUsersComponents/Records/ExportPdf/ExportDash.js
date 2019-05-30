5b123c6c8ebd2129828337c253265e3b3667739e
wabens <waltkbenson@gmail.com>
2019-05-30 13:29:36 -0500

    hooked up to select category

--- src/components/AllUsersComponents/Records/ExportPdf/ExportDash.js
+++ src/components/AllUsersComponents/Records/ExportPdf/ExportDash.js
@@ -13,16 +13,6 @@ import Button from '@material-ui/core/Button';
 
 
 
-var docDef = {
-    content: [
-        'First paragraph',
-        'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
-    ]
-
-}
-
-
-
 class ExportDash extends Component {
 
 
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -28,7 +28,7 @@ import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import theme from './theme';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRecycle, faCarrot, faTint, faPen, faPlus, faSeedling, faHome, faTractor, faClipboard, faUsers, faTable, faHorse, faIdCard, faVial, faSignOutAlt, faThermometerThreeQuarters, faTrashAlt, faMinusCircle } from '@fortawesome/free-solid-svg-icons';
+import { faRecycle, faCarrot, faTint, faPen, faPlus, faSeedling, faHome, faTractor, faClipboard, faUsers, faTable, faHorse, faIdCard, faVial, faSignOutAlt, faThermometerThreeQuarters, faTrashAlt, faMinusCircle, faFileExport } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 import ManageWorker from '../AdminComponents/ManageRoles/ManageWorker';
 
@@ -63,7 +63,7 @@ import RecordHarvestDashboard from '../AllUsersComponents/Records/SelectHarvestR
 import RecordCropsFieldsLabelCode from '../AllUsersComponents/Records/CropsFieldsLabelCodeRecord/CropsFieldsLabelCodeRecord';
 import ExportDash from '../AllUsersComponents/Records/ExportPdf/ExportDash.js';
 
-library.add(faHome,faCarrot, faRecycle, faPen, faPlus, faSeedling, faTint, faTractor, faClipboard, faUsers, faTable, faHorse, faIdCard, faVial, faSignOutAlt, faThermometerThreeQuarters, faTrashAlt, faMinusCircle);
+library.add(faHome,faCarrot, faRecycle, faPen, faPlus, faSeedling, faTint, faTractor, faClipboard, faUsers, faTable, faHorse, faIdCard, faVial, faSignOutAlt, faThermometerThreeQuarters, faTrashAlt, faMinusCircle, faFileExport);
 
 class App extends Component {
   componentDidMount () {
