5e665b8d7c2675a1c8cc5bc2739798ceee16b626
Chris Borgen <borgen_12@hotmail.com>
2019-05-31 13:32:09 -0500

    pushing again

--- src/components/AdminComponents/HarvestYear/EditHarvestYear/EditManureCompost/Manure/Manure.js
+++ src/components/AdminComponents/HarvestYear/EditHarvestYear/EditManureCompost/Manure/Manure.js
@@ -73,7 +73,7 @@ class Manure extends Component {
                 }
             },
         })
-        if (event.target.value === '') {
+       /*  if (event.target.value === '') {
             this.setState({
                 disable: true
             })
@@ -82,7 +82,7 @@ class Manure extends Component {
             this.setState({
                 disable: false
             })
-        }
+        } */
     }
 
     componentDidMount = () => {
