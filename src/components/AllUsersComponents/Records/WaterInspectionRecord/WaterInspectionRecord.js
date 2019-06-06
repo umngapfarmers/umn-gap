9abe2a08280f1bec86a8f5c5d5673cdd59b4c5ba
Lili Bourgeois <lbourgeois90@gmail.com>
2019-06-06 13:36:09 -0500

    add comments to waterinspectionrecord

--- src/components/AllUsersComponents/Records/WaterInspectionRecord/WaterInspectionRecord.js
+++ src/components/AllUsersComponents/Records/WaterInspectionRecord/WaterInspectionRecord.js
@@ -15,11 +15,13 @@ class WaterInspectionRecord extends Component {
   state= {
     selectHarvestYear: '',
   }
-
+   
+  //FUNCTION-- on initialization of page-- dispatch GET_HARVEST_YEAR to get harvest years from DB , store them in reducer, populate drop down menu
   componentDidMount() {
     this.props.dispatch({type:'GET_HARVEST_YEAR'});
   }
 
+   //FUNCTION- handles change of text fields-- sets state to user inputed values 
   handleChange = propertyName => {
     return event => {
       this.setState({
@@ -29,10 +31,13 @@ class WaterInspectionRecord extends Component {
       }
     }
   
+    //FUNCTION - on click of submit button- prevents refresh of page-- dispatches payload of state to database to get data based on selected harvest year
     handleSubmit = () => {
       this.props.dispatch({type:'GET_RECORD_WATER_INSPECT', payload: this.state.selectHarvestYear})
     }
 
+    //FUNCTION- validation- conditionally renders submit button based on status of state-- if state contains values then submit button is enabled,
+    // if state does not contain values then submit button is disabled (user cannot submit)
     handleError = () => {
       if(this.state.selectHarvestYear !== ''){
         return (
@@ -47,9 +52,6 @@ class WaterInspectionRecord extends Component {
         )
       }
     }
-
-
-
 
   render() {
     const {classes} = this.props;
