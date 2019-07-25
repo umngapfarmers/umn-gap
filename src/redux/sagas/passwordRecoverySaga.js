e24824fd575cd252a26900d0bdaedec23e9e2054
Lili Bourgeois <lbourgeois90@gmail.com>
2019-07-24 20:29:58 -0500

    create passwordrecoverysaga

---
+++ src/redux/sagas/passwordRecoverySaga.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+
+function* checkEmailPost(action) {
+    try {
+        yield axios.get(`/checkEmail/?email=${action.payload}`);
+    }
+    catch (error) {
+        alert(`Sorry, that email does not exist. Try again!`);
+    }
+}
+
+
+function* passwordRecoverySaga() {
+  yield takeLatest('CHECK_EMAIL', checkEmailSaga);
+  
+}
+
+export default passwordRecoverySaga;
