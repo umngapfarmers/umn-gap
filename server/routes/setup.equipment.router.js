c3f99b2227d73e19c3668ca1fed1b5c963572be9
wabens <waltkbenson@gmail.com>
2019-07-30 14:03:54 -0500

    working route for new pest

--- server/routes/setup.equipment.router.js
+++ server/routes/setup.equipment.router.js
@@ -7,25 +7,6 @@ const {
 
 
 
-// router.get('/', rejectUnauthenticated, (req, res) => {
-//   let sqlText = `SELECT * FROM "farm_manure" 
-//         JOIN "label_code" on "farm_manure"."label_code_id" = "label_code"."label_code_id"
-//         WHERE "farm_manure"."harvest_year_id" = $1 AND "farm_manure_status" = TRUE;`
-//   let harvest_id = req.user.current_harvest_year;
-//   console.log(`harvest id off user `, harvest_id);
-
-//   pool.query(sqlText, [harvest_id])
-//     .then((result) => {
-//       // console.log(`result from manure GET`, result.rows);
-//       res.send(result.rows)
-//     })
-//     .catch((error) => {
-//       console.log(`error getting manure`, error);
-//       res.sendStatus(500);
-//     })
-// });
-
-
 router.post('/new/farm_tool', (req, res) => {
 
   let sqlText = `INSERT INTO "farm_tool" ("farm_tool_name") VALUES ($1);`
@@ -79,83 +60,24 @@ router.post('/new/firstaid', (req, res) => {
       res.sendStatus(500);
     })
 });
-// router.post('/new', rejectUnauthenticated, (req, res) => {
 
-//   let sqlText = `INSERT INTO "farm_manure"
-//         ("farm_manure_date", "farm_manure_description", "farm_manure_rate", "label_code_id", "harvest_year_id")
-//         VALUES ($1, $2, $3, $4, $5);`;
-//   let values = [
-//     req.body.date,
-//     req.body.description,
-//     req.body.rate,
-//     req.body.label_code,
-//     req.user.current_harvest_year,
-//   ];
+router.post('/new/pest', (req, res) => {
 
+  let sqlText = `INSERT INTO "farm_pest" ("farm_pest_type", "farm_pest_location") VALUES ($1, $2);`
+  let values = [
+    req.body.farm_pest_type,
+    req.body.farm_pest_location
+  ];
 
-
-//   pool.query(sqlText, values)
-//     .then((result) => {
-//       console.log('added manure source ', result.rows[0]);
-
-//       res.send(result.rows[0]);
-//     })
-//     .catch((error) => {
-//       console.log(`error in farm_manure post `, error);
-//       res.sendStatus(500);
-//     })
-// });
-
-// router.delete('/:id', rejectUnauthenticated, (req, res) => {
-//   let sqlText = `DELETE FROM "farm_manure" WHERE "farm_manure_id" = $1`;
-//   let manure_id = req.params.id;
-//   pool.query(sqlText, [manure_id])
-//     .then((result) => {
-//       console.log('deleted manure source ');
-//       res.sendStatus(200);
-//     })
-//     .catch((error) => {
-//       console.log(`error in farm_manure delete `, error);
-//       res.sendStatus(500);
-//     })
-
-// })
-
-// router.put('/edit', rejectUnauthenticated, (req, res) => {
-
-//   const date = req.body.farm_manure_date
-//   const name = req.body.farm_manure_description
-//   const label = req.body.label_code_id
-//   const rate = req.body.farm_manure_rate
-//   const id = req.body.farm_manure_id
-
-//   const queryText = 'UPDATE "farm_manure" SET "farm_manure_date"=$1, "farm_manure_description"=$2, "label_code_id"=$3, "farm_manure_rate"=$4 WHERE farm_manure_id=$5';
-//   pool.query(queryText, [date, name, label, rate, id])
-//     .then(() => {
-//       res.sendStatus(200);
-//     })
-//     .catch((err) => {
-//       console.log('Error deleting field query', err);
-//       res.sendStatus(500);
-//     });
-// });
-
-// router.put('/disable', rejectUnauthenticated, (req, res) => {
-
-//   const id = req.body.checked
-//   console.log('checked is', req.body);
-//   for (let num of id) {
-//     const queryText = 'UPDATE "farm_manure" SET "farm_manure_status"= FALSE WHERE farm_manure_id=$1';
-//     pool.query(queryText, [num])
-//       .then(() => {
-//         res.sendStatus(200);
-//       })
-//       .catch((err) => {
-//         console.log('Error deleting crop query', err);
-//         res.sendStatus(500);
-//       });
-//   }
-// });
-
+  pool.query(sqlText, values)
+    .then((result) => {
+      console.log('added pest', result.rows[0]);
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log(`error in pest post `, error);
+      res.sendStatus(500);
+    })
+});
 
 module.exports = router;
