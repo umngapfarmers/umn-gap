7a154ef65e3ac7ccc8508bf2598cf1923bdb0376
Lili Bourgeois <lbourgeois90@gmail.com>
2019-07-31 12:55:09 -0500

    create get route and insert bathroom log into router

---
+++ server/routes/log.bathroom.router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+/**
+ * GET route template
+ */
+
+router.get('/', rejectUnauthenticated, (req, res) => {
+    console.log('IN GET BATHROOM')
+    console.log('Harvest year is:', req.user.current_harvest_year);
+    let sqlQuery = `SELECT * FROM "farm_bathroom" WHERE "harvest_year_id" = $1;`
+    pool.query(sqlQuery, [req.user.current_harvest_year])
+        .then((response) => {
+            console.log(`Bathroom information`, response.rows);
+            res.send(response.rows)  
+        })
+        .catch((error) => {
+            console.log(`ERROR in GET BATHROOM`, error);
+            res.sendStatus(500);
+        })
+});
+
+
+
+module.exports = router;
