9e1de4721dbeffec90498cc34275907922285b50
wabens <waltkbenson@gmail.com>
2019-08-02 16:38:36 -0500

    find replace tool - thermometer router

--- server/routes/equipmentRoutes/equipment.thermometer.router.js
+++ server/routes/equipmentRoutes/equipment.thermometer.router.js
@@ -9,26 +9,26 @@ router.post('/new', (req, res) => {
   console.log(`in post `, req.body);
 
   let harvest_id = req.user.current_harvest_year;
-  let sqlText = `INSERT INTO "farm_tool" ("farm_tool_name", "harvest_year_id") VALUES ($1, $2);`
+  let sqlText = `INSERT INTO "farm_thermometer" ("farm_thermometer_location", "harvest_year_id") VALUES ($1, $2);`
   let values = [
-    req.body.farm_tool_name,
+    req.body.farm_thermometer_location,
     harvest_id
   ];
 
   pool.query(sqlText, values)
     .then((result) => {
-      console.log('added farm tool ');
+      console.log('added farm thermometer ');
       res.sendStatus(201);
     })
     .catch((error) => {
-      console.log(`error in farm_tool post `, error);
+      console.log(`error in farm_thermometer post `, error);
       res.sendStatus(500);
     })
 });
 
 router.get('/', rejectUnauthenticated, (req, res) => {
   let harvestYear = req.user.current_harvest_year;
-  let sqlQuery = `SELECT * FROM "farm_tool" WHERE "harvest_year_id" = $1;`
+  let sqlQuery = `SELECT * FROM "farm_thermometer" WHERE "harvest_year_id" = $1;`
   pool.query(sqlQuery, [harvestYear])
     .then(result => {
       res.send(result.rows);
@@ -40,7 +40,7 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 });
 
 router.delete('/delete/:id', rejectUnauthenticated, (req, res) => {
-  const queryText = 'DELETE FROM "farm_tool" WHERE "farm_tool_id" = $1;';
+  const queryText = 'DELETE FROM "farm_thermometer" WHERE "farm_thermometer_id" = $1;';
   pool.query(queryText, [req.params.id])
     .then(() => {
       res.sendStatus(200);
