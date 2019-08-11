1c856f2faf19f695176f7a77d7c862d99b9e4faa
Lili Bourgeois <lbourgeois90@gmail.com>
2019-08-11 12:30:47 -0500

    revise first aid table

---
+++ src/components/AllUsersComponents/Records/EquipmentFirtAidRecord/EquipmentFirstAidTable.js
@@ -0,0 +1,90 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
+const moment = require('moment');
+
+
+
+class EquipmentFirstAidTable extends Component {
+
+
+      //FUNCTION- toggles the check of the checkbox
+      displayFirstAidStocked = (firstaid_stocked) => {
+        if (firstaid_stocked == true){
+            return (
+            <Typography>Yes</Typography>
+            )
+        }
+    
+        else if (firstaid_stocked == false){
+            return (
+                <Typography>No</Typography>
+                )
+        }
+    }
+
+
+  //maps through reducer to create table that displays selected harvest year data based on selected record
+  render() {
+    const { classes} = this.props;
+    return (
+        <div style={{overflow : 'auto',fontSize: '14px'}}>
+        <Table className={classes.table}
+        padding='dense'
+        size='small'>
+        <TableHead>
+          <TableRow hover='true'>
+            <TableCell align="center" className={classes.tableFontBody}>Date</TableCell>
+            <TableCell align="center"  className={classes.tableFontBody}>Location</TableCell>
+            <TableCell align="center"  className={classes.tableFontBody}>Stocked</TableCell>
+            <TableCell align="center"  className={classes.tableFontBody}>Comments</TableCell>
+            <TableCell align="center"  className={classes.tableFontBody}>Signature</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {this.props.reduxState.recordFirstaid.map(row => (
+            <TableRow key={row.firstaid_id} hover='true'>
+              <TableCell align="left" scope="row" className={classes.tableFontAndBorder}>{moment(row.firstaid_date).format('MM-DD-YYYY')}</TableCell>
+              <TableCell align="left"  className={classes.tableFontAndBorder}>{row.farm_firstaid_location}</TableCell>
+              <TableCell align="left"  className={classes.tableFontAndBorder}>{this.displayFirstAidStocked(row.firstaid_stocked)}</TableCell>
+              <TableCell align="left"  className={classes.tableFontAndBorder}>{row.firstaid_comment}</TableCell>
+              <TableCell align="left"  className={classes.tableFontAndBorder}>{row.person_first} {row.person_last}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+    );
+  }
+}
+
+const styles = theme => ({
+  container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+  },
+  tableFontBody:{
+    fontSize: 12,
+  },
+  tableFontAndBorder:{
+    fontSize: 12,
+    borderRight: '1px solid rgba(224, 224, 224, 1)',
+    borderLeft: '1px solid rgba(224, 224, 224, 1)',
+    borderCollapse: 'collapse',
+  }
+});
+
+
+
+
+const mapReduxStateToProps = (reduxState) => ({
+  reduxState,
+});
+
+export default connect( mapReduxStateToProps )(withStyles(styles)(EquipmentFirstAidTable));
