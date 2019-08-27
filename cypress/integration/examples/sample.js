e7493148347bd7fac453d4591243d982f800aa03
wabens <waltkbenson@gmail.com>
2019-08-27 14:52:56 -0500

    Revert "incorporates random username generator package"
    This reverts commit 29237e0597d3d84a845952dd7d3c3d6a412736f2.

--- cypress/integration/examples/sample.js
+++ cypress/integration/examples/sample.js
@@ -1,6 +1,3 @@
-var rug = require('random-username-generator');
-var new_username = rug.generate();
-
 describe('Registration test', function () {
   it('Finds the content Register button and clicks it', function () {
     cy.visit('http://localhost:3000/#/home')
@@ -8,8 +5,8 @@ describe('Registration test', function () {
     cy.get(':nth-child(1) > :nth-child(1) > .MuiFormControl-root-102 > .MuiInputBase-root-137 > .MuiInputBase-input-147').type('1086420')
     cy.get(':nth-child(2) > .MuiFormControl-root-102 > .MuiInputBase-root-137 > .MuiInputBase-input-147').type('Walter')
     cy.get(':nth-child(3) > .MuiFormControl-root-102 > .MuiInputBase-root-137 > .MuiInputBase-input-147').type('Benson')
-    cy.get(':nth-child(4) > .MuiFormControl-root-102 > .MuiInputBase-root-137 > .MuiInputBase-input-147').type(new_username)
-    cy.get(':nth-child(5) > .MuiFormControl-root-102 > .MuiInputBase-root-137 > .MuiInputBase-input-147').type(new_username)
+    cy.get(':nth-child(4) > .MuiFormControl-root-102 > .MuiInputBase-root-137 > .MuiInputBase-input-147').type('BensonFarm')
+    cy.get(':nth-child(5) > .MuiFormControl-root-102 > .MuiInputBase-root-137 > .MuiInputBase-input-147').type('BensonFarm')
     cy.get(':nth-child(6) > .MuiFormControl-root-102 > .MuiInputBase-root-137 > .MuiInputBase-input-147').type('waltkbenson@gmail.com')
     cy.get(':nth-child(7) > :nth-child(1) > .MuiFormControl-root-102 > .MuiInputBase-root-137 > .MuiInputBase-input-147').type('Benson Farm')
     cy.get(':nth-child(8) > :nth-child(1) > .MuiFormControl-root-102 > .MuiInputBase-root-137 > .MuiInputBase-input-147').type('Some Road')
@@ -17,7 +14,6 @@ describe('Registration test', function () {
     cy.get(':nth-child(10) > :nth-child(1) > .MuiFormControl-root-102 > .MuiInputBase-root-137 > .MuiInputBase-input-147').type('Ohio')
     cy.get(':nth-child(11) > :nth-child(1) > .MuiFormControl-root-102 > .MuiInputBase-root-137 > .MuiInputBase-input-147').type('55555')
     cy.contains('Register').click()
-    
 
 
 
